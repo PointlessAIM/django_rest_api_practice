e0acf7caa60fbbe1bed821c0528194af5b71a4d1
PointlessAIM <89607813+PointlessAIM@users.noreply.github.com>
2023-01-09 10:11:15 -0500

    add quick fix and make some tests

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,4 +5,6 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model= Project
         fields= ('id', 'title', 'description', 'technology', 'creation_date')
-        read_only_fields=('creation_date')
+        #Recordar poner la coma al final, de esta forma, python reconoce que es una tupla,
+        #en el caso contrario, lo lee como string.
+        read_only_fields=('creation_date',) 
